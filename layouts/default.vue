351225b70630734735b7a00069b3467319d9d4ac
Virgile-ifa <52604808+Virgile-Ifa@users.noreply.github.com>
2020-06-25 19:24:06 +0200

    layout update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 
 <template>
@@ -5,6 +6,17 @@
         <nuxt />
         <div class="navbar">  
           <ul>
+=======
+<template>
+  <div class="content" id="anchor1">
+
+        <nuxt />
+        
+
+        <a class="retour"><nuxt-link :to="{path: '/', hash: '#anchor1'}" class="button">^</nuxt-link></a>
+        <div class="barnav">  
+          <ul class="navicone">
+>>>>>>> Stashed changes
             <li>
               <nuxt-link to="/">
               <i class="fa fa-home" aria-hidden="true"></i>
@@ -30,7 +42,17 @@
       </div> 
 </template>
 
+<<<<<<< Updated upstream
 <style>
+=======
+<style langue="css">
+
+body{
+  /* background-color: lightblue; */
+  height: 10000px;
+  }
+.barnav .navicone{
+>>>>>>> Stashed changes
 
 .navbar ul{
  overflow: hidden;
@@ -42,7 +64,36 @@
     
 }
 
+<<<<<<< Updated upstream
 ul{ 
+=======
+       .retour, .retour:visited{
+         position: fixed;
+         right: 5px;
+         bottom:25.5vh;
+         text-decoration: none;
+       }   
+
+       .button, .button:visited{
+         background-color: white;
+         color: lightblue;
+         font-size: 1.8em;
+         
+         
+       }
+
+
+       .button:hover{
+         background-color: lightblue;
+         color: white;
+         text-decoration: none;
+         }
+
+
+.navicone{ 
+  margin-left: 0;
+    height:25vh;
+>>>>>>> Stashed changes
     padding: 5px;
     display: flex; 
     flex-direction : row; 
