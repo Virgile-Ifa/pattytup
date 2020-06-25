a9facc2abeaffe6d9e5fdd7c8b073e13e172d6c0
Virgile-ifa <52604808+Virgile-Ifa@users.noreply.github.com>
2020-06-25 12:18:15 +0200

    suppression nom catégorie
    suppression nom catégorie

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,22 +6,22 @@
           <ul>
             <li>
               <nuxt-link to="/">
-              <i class="fa fa-home" aria-hidden="true">Home</i>
+              <i class="fa fa-home" aria-hidden="true"></i>
               </nuxt-link>
             </li>
             <li>
                <nuxt-link to="/">
-              <i class="fa fa-cutlery" aria-hidden="true">Produit</i>
+              <i class="fa fa-cutlery" aria-hidden="true"></i>
               </nuxt-link>
             </li>
             <li>
                <nuxt-link to="/">
-              <i class="fa fa-address-book-o" aria-hidden="true">Recette</i>
+              <i class="fa fa-book" aria-hidden="true"></i>
               </nuxt-link>
             </li>
             <li>
                <nuxt-link to="/">
-              <i class="fa fa-address-card-o" aria-hidden="true">Contact</i>
+              <i class="fa fa-address-card-o" aria-hidden="true"></i>
               </nuxt-link>
             </li>
           </ul>
